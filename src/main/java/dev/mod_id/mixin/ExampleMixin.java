ece6d26152c272a803c1010562f312bb888d1eb0
stlouisn <stlouisn@hotmail.com>
2024-09-12 10:44:56 -0300

    Create ExampleMixin.java

---
+++ src/main/java/dev/mod_id/mixin/ExampleMixin.java
@@ -0,0 +1,12 @@
+package dev.mod_id.mixin;
+
+import dev.mod_id.config.ModConfig;
+import dev.mod_id.config.ModConfigData;
+
+@Mixin(PackageName.class)
+public class ExampleMixin {
+
+  // to access config variable value use:
+  // ModConfig.CONFIG.variable_name
+  
+}
