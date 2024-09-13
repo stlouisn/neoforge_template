fe4bf1afb90ac13c4a6740806db36fc1d861fc5f
stlouisn <stlouisn@hotmail.com>
2024-09-13 14:02:50 -0300

    Update ModName.java

--- src/main/java/dev/mod_id/ModName.java
+++ src/main/java/dev/mod_id/ModName.java
@@ -1,6 +1,10 @@
 package dev.mod_id;
 
+import dev.mod_id.config.ConfigHandler;
+import dev.mod_id.utils.ModConstants;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
