15e9c1069d043c43c9c2effa77687911526620f0
stlouisn <stlouisn@hotmail.com>
2025-01-06 13:26:10 -0400

    Update ModNameClient.java

--- src/main/java/dev/mod_id/ModNameClient.java
+++ src/main/java/dev/mod_id/ModNameClient.java
@@ -1,17 +1,16 @@
 package dev.mod_id;
 
-import dev.mod_id.utils.Constants;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@SuppressWarnings("unused")
-@Mod(value = Constants.MOD_ID, dist = Dist.CLIENT)
+@Mod(value = MOD_ID, dist = Dist.CLIENT)
 public final class ModNameClient {
 
   public ModNameClient(ModContainer container) {
     container.registerExtensionPoint(IConfigScreenFactory.class, (mc, parent) -> new ConfigurationScreen(container, parent));
   }
+  
 }
