479f75d4263b6bef1af78c099fd377289b077294
stlouisn <stlouisn@hotmail.com>
2025-01-06 13:25:38 -0400

    Update ModName.java

--- src/main/java/dev/mod_id/ModName.java
+++ src/main/java/dev/mod_id/ModName.java
@@ -1,20 +1,22 @@
 package dev.mod_id;
 
 import dev.mod_id.config.ConfigHandler;
-import dev.mod_id.utils.Constants;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SuppressWarnings("unused")
 @Mod(Constants.MOD_ID)
 public final class ModName {
 
-  public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
+	public static final String MOD_ID = "anvilrestoration";
+
+	@SuppressWarnings("unused")
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
   public ModName(ModContainer container) {
     container.registerConfig(Type.COMMON, ConfigHandler.configSpec);
   }
+  
 }
