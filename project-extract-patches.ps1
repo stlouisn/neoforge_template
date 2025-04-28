6d8fad5a3e7b0249999e2a01c11289b5f3dbb5ae
stlouisn <stlouisn@hotmail.com>
2025-04-28 18:30:42 -0300

    neoforge-template

--- project-extract-patches.ps1
+++ project-extract-patches.ps1
@@ -1,10 +1,22 @@
-# Set mod_patches directory
-$patchesDirectory = "mod_patches\mezz\jei\gui\overlay"
+# Set source directory
+$copyFromPath = "D:\Git Projects\just_enough_items\gui\build\classes\java\main\mezz\jei\gui\overlay"
 
-# Create the mod_patches directory if it doesn't exist
-If (!(Test-Path -Path $patchesDirectory)) {
-    New-Item -Path $patchesDirectory -ItemType Directory
+# Set destination directory
+$copyToPath = "D:\Git Projects\just_enough_items\mod_patches\mezz\jei\gui\overlay"
+
+# Set source filename
+$copyFileName = "ConfigButton.class"
+
+# Create destination directory if it doesn't exist
+If (!(Test-Path -Path $copyToPath)) {
+    New-Item -Path $copyToPath -ItemType Directory
 }
 
+# Fix the destination directory
+$copyToPath = $copyToPath + "\."
+
+# combine the source directory and filename
+$copyFileName = $copyFromPath + "\" + $copyFileName
+
 # Copy the class files from the build directory to the mod_patches directory
-Copy-Item 'D:\Git Projects\just_enough_items\gui\build\classes\java\main\mezz\jei\gui\overlay\ConfigButton.class' 'D:\Git Projects\just_enough_items\mod_patches\mezz\jei\gui\overlay\.' -Force
+Copy-Item $copyFileName $copyToPath -Force
